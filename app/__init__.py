5ca03dd237a35d26fb5ce27a927e4a5454ebbaf4
Sachin Pande <sazap10@gmail.com>
2014-02-28 23:42:06 +0000

    added base and nav bar templates with basic index

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
-from flask import Flask
+from flask import Flask, url_for
+
+def configure_jinja(app):
+    @app.context_processor
+    def inject_static():
+        return dict(STATIC_URL=url_for('static', filename=''))
 
 app = Flask(__name__)
+configure_jinja(app)
 from app import views
